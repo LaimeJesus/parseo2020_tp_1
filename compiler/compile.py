cca68e555e2b55bdfdb3115df24ba83e8d7547c6
lucio <franciolucio@gmail.com>
2020-12-14 22:36:14 -0300

    Agrego la funciones que faltan al compile

--- compiler/compile.py
+++ compiler/compile.py
@@ -53,6 +53,27 @@ int main() {
 ''')
 
     @staticmethod
+    def getTag(constructor):
+        if not (constructor in cons.cons):
+            cons.addCons(constructor)
+        return cons.cons.index(constructor)
+
+    @staticmethod
+    def compile(data):
+        Compiler.ast = json.loads(data)
+        Compiler.loadFunctions()
+        f = Compiler.functions()
+        p = Compiler.prototypes()
+        d = dict(prototypes=p, functions=f, checks=Compiler.checks(), prints=Compiler.prints(), main=Compiler.main())
+        d['print'] = Compiler.compilePrint()
+        Compiler.output = Compiler.template.substitute(d)
+
+    @staticmethod
+    def loadFunctions():
+        for f in Compiler.ast[1]:
+            Compiler.funs.append(fun.Fun(f[1], f[2], f[3], f[4], f[5]))
+            
+    @staticmethod
         def compilePrint():
             c = ['case %d: c = "%s"; break;' % (i, c) for i, c in enumerate(cons.cons)]
             return '''
@@ -67,6 +88,34 @@ int main() {
             cout << ")";
         }}
     '''.format('\n\t'.join(c))
+
+    @staticmethod
+    def prototypes():
+        return ''.join([f.prototype() for f in Compiler.funs])
+
+    @staticmethod
+    def functions():
+        return ''.join([f.compile() for f in Compiler.funs])
+
+    @staticmethod
+    def checks():
+        return '//TODO: checks'
+
+    @staticmethod
+    def prints():
+        return '//TODO: prints'
+
+    @staticmethod
+    def main():
+        main = ''
+        for print in Compiler.ast[3]:
+            expression = rule.Expression(print[1])
+            main += '''
+        // %r''' % print[1]
+            main += expression.compile()
+            main += '''
+        printTerm(%s); cout << endl;''' % expression.var
+        return main
 
 if __name__ == '__main__':
     filename = sys.argv[1]
